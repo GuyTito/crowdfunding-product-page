2aafcd525f87f8137dc6c5763f2ddb7d0a91bf8e
GuyTito <35842294+GuyTito@users.noreply.github.com>
2022-02-02 11:50:34 +0000

    click on reward buttons to scroll to checked selection modal product

--- main.js
+++ main.js
@@ -36,3 +36,25 @@ radio_btns.forEach((radio) => {
     pledge.classList.remove('hidden')
   })
 })
+
+
+const reward_btns = document.querySelectorAll('.reward')
+reward_btns.forEach((reward) => {
+  reward.addEventListener('click', (e) => {
+    // open selection modal
+    selection_modal.style.display = "block"
+    // scroll to product
+    document.getElementById(`${reward.dataset.reward}_product`).scrollIntoView({behavior: "smooth", block: "start", inline: "center"})
+    // check product
+    document.querySelector(`input[value=${reward.dataset.reward}]`).checked = true
+    // hide all pledges
+    radio_btns.forEach((radio) => {
+      let pledge = document.querySelector(`#${radio.value}`)
+      if (pledge.classList.contains("hidden")) return
+      else pledge.classList.add('hidden')
+    })
+    // show selected pledge
+    let pledge = document.querySelector(`#${reward.dataset.reward}`)
+    pledge.classList.remove('hidden')
+  })
+})
